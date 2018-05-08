1bc6bd1b74b269d4111e75cfceab32e79f42a3b9
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 12:42:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else array.length > 2
+    array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
